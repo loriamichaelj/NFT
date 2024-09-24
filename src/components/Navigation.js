31f5be9fea6c4c8bda03719b1d4dffe0feb7eafa
MJ <loriamichaelj@gmail.com>
2024-09-24 15:36:27 -0700

    updates ui

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,14 +5,16 @@ import logo from "../logo.png";
 const Navigation = ({ account }) => {
   return (
     <Navbar className="my-3">
-      <img
-        alt="logo"
-        src={logo}
-        width="40"
-        height="40"
-        className="d-inline-block align-top mx-3"
-      />
-      <Navbar.Brand href="#">Dapp Punks</Navbar.Brand>
+      <a href="/">
+        <img
+          src={logo}
+          alt="logo"
+          width="134.5"
+          height="61"
+          className="d-inline-block align-top mx-3"
+        />
+      </a>
+      <Navbar.Brand href="#"></Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text>{account}</Navbar.Text>
       </Navbar.Collapse>
